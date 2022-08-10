dd1bf1a075e297a360bbd027da4110935ebd3c10
Ngoc Vu <SUN-ASTERISK\vu.thi.ngoc@b120484-lt.sun-asterisk.com>
2022-08-10 10:34:48 +0700

    Create multi layout(admin, user)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :check_role_user
+
+  private
+  # check la admin thi se ve site admin
+
+  def check_role_user
+    # if current_user.admin?
+    if true
+      redirect_to admin_root_path
+    end
+  end
 end
