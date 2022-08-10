4262328cd472b0e8cb3266c088b9660953344889
Ngoc Vu <SUN-ASTERISK\vu.thi.ngoc@b120484-lt.sun-asterisk.com>
2022-08-10 09:59:48 +0700

    add toaster

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,26 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 require("jquery")
 import "bootstrap"
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+global.toastr = require("toastr")
+
+toastr.options = {
+  "closeButton":  false,
+  "positionClass": "toast-bottom-center",
+  "preventDuplicates": false,
+  // action khi click vào thông báo
+  "onclick": null,
+  // thời gian, hiệu ứng hiển thị và ẩn
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
